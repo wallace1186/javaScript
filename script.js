fabfdf80db7d24c3805d751c549c95a378cbfcae
wallace1186 <128391478+wallace1186@users.noreply.github.com>
2024-02-10 20:00:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = prompt("insira seu nome:")
+let anoNascimento = parseInt (prompt("Qual o ano de nascimento ?" ))
+let idade = 2024 -  anoNascimento
+
+console.log (idade)
+
+alert ( `  olá meu nome é ${nome}, e tenho ${idade}  anos. `  )
